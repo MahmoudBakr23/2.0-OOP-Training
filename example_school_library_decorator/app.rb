2d9c0b167c3d48a253cb503dd596d6bdbb24016f
david bassey <davidosky1@gmail.com>
2021-09-17 10:03:45 +0100

    Create new methods for output and validation

--- example_school_library_decorator/app.rb
+++ example_school_library_decorator/app.rb
@@ -34,12 +34,8 @@ class App
   # Create a person
   def create_person
     validate_person
-
-    person =
-      Student.new(@age, @name, @parent_permission)
-    Teacher.new(@age, @specialization, @name)
-
-    @people << person
+    @people << Student.new(@age, @name, @parent_permission)
+    @people << Teacher.new(@age, specialization, @name)
     puts 'Person created successfully'
   end
 
@@ -48,70 +44,74 @@ class App
   def validate_person
     print 'Do you want to create a student (1) or a teacher (2)? [Input the number]: '
     @person_type = gets.chomp
-    case @person_type
-    when @person_type != '1' && @person_type != '2'
+    if @person_type != '1' && @person_type != '2'
       puts 'Invalid option'
       nil
-    when @person_type == '1'
-      print 'Has parent permission? [Y/N]: '
-      @parent_permission = parent_permission.downcase == 'y'
-
-    when @person_type == '2'
-      print 'Specialization: '
-      @specialization = gets.chomp
     end
     print 'Age: '
     @age = gets.chomp
 
     print 'Name: '
     @name = gets.chomp
-    @parent_permission = gets.chomp
+    if @person_type == '1'
+      print 'Has parent permission? [Y/N]: '
+      @parent_permission = gets.chomp
+      @parent_permission = @parent_permission.downcase == 'y'
+    elsif @person_type == '2'
+      print 'Specialization: '
+      @specialization = gets.chomp
+    end
   end
 
   # Create a book
   def create_book
+    validate_book
+    @books << Book.new(@title, @author)
+    puts 'Book created successfully'
+  end
+
+  def validate_book
     print 'Title: '
-    title = gets.chomp
+    @title = gets.chomp
 
     print 'Author: '
-    author = gets.chomp
-
-    @books << Book.new(title, author)
-    puts 'Book created successfully'
+    @author = gets.chomp
   end
 
   # Create a rental
   def create_rental
-    puts 'Select a book from the following list by number'
+    validate_rental
     @books.each_with_index do |book, index|
       puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}"
     end
-
-    book_index = gets.chomp.to_i
-
-    puts "\nSelect a person from the following list by number (not id)"
     @people.each_with_index do |person, index|
       puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
     end
 
-    person_index = gets.chomp.to_i
-
-    print "\nDate: "
-    date = gets.chomp
-
-    @rentals << Rental.new(date, @books[book_index], @people[person_index])
+    @rentals << Rental.new(@date, @books[@book_index], @people[@person_index])
     puts 'Rental created successfully'
+  end
+
+  def validate_rental
+    puts 'Select a book from the following list by number'
+    @book_index = gets.chomp.to_i
+    puts "\nSelect a person from the following list by number (not id)"
+    @person_index = gets.chomp.to_i
+    print "\nDate: "
+    @date = gets.chomp
   end
 
   # List all rentals for a given person id
   def list_all_rentals_for_person_id
-    print 'ID of person: '
-    id = gets.chomp.to_i
-
-    rentals = @rentals.filter { |rental| rental.person.id == id }
+    rentals = @rentals.filter { |rental| rental.person.id == @id }
     puts 'Rentals:'
     rentals.each do |rental|
       puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
     end
+  end
+
+  def validate_rentals_id
+    print 'ID of person: '
+    @id = gets.chomp.to_i
   end
 end
